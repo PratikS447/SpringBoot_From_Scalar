afb6ea56ade688fe8bc94ba2bd1ca99dc20e5ab0
PratikS447 <shindepratik447@gmail.com>
2026-08-15 00:52:39 +0530

    Fixed product API urls and deserialization

--- src/main/java/ecommerce/product/exceptions/NotFoundException.java
+++ src/main/java/ecommerce/product/exceptions/NotFoundException.java
@@ -6,6 +6,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(code = HttpStatus.BAD_REQUEST)
 public class NotFoundException extends Exception{
     public NotFoundException(){
-        super("The product ID was not found.");
+        super("The product id is not found");
     }
 }
